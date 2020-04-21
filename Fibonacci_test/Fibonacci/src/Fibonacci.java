5b9249e5f1a9b5b69eb63eddf2c0eac03e541384
yeranyeran <ran_ye@stu.xjtu.edu.cn>
2020-04-22 02:15:59 +0800

    first commit

---
+++ Fibonacci_test/Fibonacci/src/Fibonacci.java
@@ -0,0 +1,14 @@
+public class Fibonacci {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 200; i++) {
+            System.out.println("Fibonacci.of(" + i + ") == " + fibonacci(i));
+        }
+    }
+
+    public static long fibonacci(int i) {
+        if(i == 1 || i == 2) {
+            return 1;
+        }
+        return fibonacci(i - 1) + fibonacci(i - 2);
+    }
+}
