24b3f36860af3ef5384bd982329f16ff42d4be2f
yeranyeran <57438376+yeranyeran@users.noreply.github.com>
2021-05-13 00:26:28 +0800

    Update Fibonacci.java

--- Fibonacci_test/Fibonacci/src/Fibonacci.java
+++ Fibonacci_test/Fibonacci/src/Fibonacci.java
@@ -1,6 +1,7 @@
 public class Fibonacci {
     public static void main(String[] args) {
         for (int i = 1; i <= 200; i++) {
+            System.out.println("gitee更新测试");
             System.out.println("Fibonacci.of(" + i + ") == " + fibonacci(i));
         }
     }
